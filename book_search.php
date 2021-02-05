6610c8d40fd490c29fcf7be7a55ba1e986baad27
Ayesha Khan <68498873+NAyeshaKhan@users.noreply.github.com>
2021-02-06 02:38:36 +0600

    Book files backgrounds updated

--- book_search.php
+++ book_search.php
@@ -3,25 +3,23 @@
 
 <style>
 body{
-background-color:rgb(215,215,245);
+background-image: url(YqOe62k.png);
 }
 </style>
 
-<h1 style="background-color:rgb(215,195,250);">Search for Books:</h1>
+<h1 style="background-color:rgb(83, 207, 180);">Search for Books:</h1>
 <body>
 
 <form action="book_search.php" method="POST">
 
-
 <fieldset>	
 <legend>Search by Genre:</legend>
-  Genre:<input type="text" name="Gen"></input><br>
+  Genre:<input type="text" name="Gen"></input>
  <input type="submit" name="SubmitGen" value="Search Genre"></input><br> 
+ <button class="bkbtn" formaction="likes_frontpage.php">Go Back to Homepage</button>
+
 </fieldset>  
-  
-  
-  
-</form>
+ </form>
 
 </body>
 </html>
@@ -33,11 +31,11 @@ background-color:rgb(215,215,245);
 	
 	$conn=mysqli_connect($hostname,$username,$pwd) or die('Database connection failed.');
 	
-	$db=mysqli_select_db($conn,'bookstore');
+	$db=mysqli_select_db($conn,'likes');
 
 if (isset($_POST['SubmitGen'])){
 	$gen=$_POST['Gen'];
-	$q="SELECT * FROM `book_info` WHERE Genre='$gen'";
+	$q="SELECT * FROM `books` WHERE Genre='$gen'";
 	$c=0;
 	if($running=mysqli_query($conn,$q)){
 		echo 'Books of ',$gen, ' genre:<br>';
@@ -45,10 +43,13 @@ if (isset($_POST['SubmitGen'])){
 	
 		while($i=mysqli_fetch_assoc($running)){
 		$c++;
-		echo $i['Book_Id'].', ';
+		echo $i['Book_Id'].' | ';
 		echo $i['Book_Name'].' | ';
-		echo $i['Author'].' | Tk.';
-		echo $i['Price'].'<br>';
+		echo $i['Author'].' | Genre: ';
+		echo $i['Genre'].' | Number Of Pages: ';
+		echo $i['PageNo'].' | Read?:	';
+		echo $i['TBRead'];
+		echo "<br>";
 		}
 		echo '<br>Total ',$c,' books found.' ;
 	}
